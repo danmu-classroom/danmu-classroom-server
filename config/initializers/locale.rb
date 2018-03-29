b2d49a69395efb8239fdfa23c4de2ac915a337e6
楊竑昕 <jk195417@gmail.com>
2018-03-29 21:04:05 +0800

    Config: default locale set to zh-TW

---
+++ config/initializers/locale.rb
@@ -0,0 +1,4 @@
+Rails.application.config.encoding = 'utf-8'
+Rails.application.config.time_zone = 'Taipei'
+Rails.application.config.i18n.available_locales = ['zh-TW', 'en']
+Rails.application.config.i18n.default_locale = 'zh-TW'
