13625dcb975c66e30215f5eb8f80168885554346
楊竑昕 <jk195417@gmail.com>
2018-04-02 15:35:21 +0800

    Room: remove #destory and strong params,  route param set to :key

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,5 +1,5 @@
 class RoomsController < ApiController
-  before_action :set_room, only: %i[show destroy]
+  before_action :set_room, only: %i[show]
 
   def index
     @rooms = Room.where(online: true).order(id: :desc)
@@ -8,29 +8,19 @@ class RoomsController < ApiController
   def show; end
 
   def create
-    @room = Room.new(room_params)
-    @room.creater_ip = request.remote_ip
-    @room.stream_ip = request.remote_ip
+    @room = Room.new do |room|
+      room.creater_ip = request.remote_ip
+      room.stream_ip = request.remote_ip
+    end
     if @room.save
       render :show, status: :created, location: @room
     else
       render json: @room.errors, status: :unprocessable_entity
     end
   end
-
-  def destroy
-    @room.online = false
-    @room.save!
-    head :no_content
-  end
-
   private
 
   def set_room
-    @room = Room.find(params[:id])
-  end
-
-  def room_params
-    # params.require(:room).permit(:stream_ip)
+    @room = Room.find_by!(key: params[:key])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root to: 'pages#index'
   # Room
-  resources :rooms, except: [:new, :edit, :update], defaults: { format: :json }
+  resources :rooms, only: %i[index show create], param: :key do
+    resources :danmus, only: %i[create]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
