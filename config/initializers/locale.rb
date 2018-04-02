7729337859a8ebc6725b9601d6e62168250f12eb
楊竑昕 <jk195417@gmail.com>
2018-04-02 15:48:02 +0800

    Initializer: set locale instead default locale

--- config/initializers/locale.rb
+++ config/initializers/locale.rb
@@ -1,4 +1,6 @@
 Rails.application.config.encoding = 'utf-8'
 Rails.application.config.time_zone = 'Taipei'
 Rails.application.config.i18n.available_locales = ['zh-TW', 'en']
-Rails.application.config.i18n.default_locale = 'zh-TW'
+Rails.application.config.i18n.locale = 'zh-TW'
+# default_locale default is 'en'
+# Rails.application.config.i18n.default_locale = 'en'
