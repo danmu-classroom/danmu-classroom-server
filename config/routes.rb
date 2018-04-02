298f3d9e849e8b7a4f828fbedcadcb51f8e69a87
楊竑昕 <jk195417@gmail.com>
2018-04-02 16:46:55 +0800

    Api: default .json format

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   root to: 'pages#index'
-  # Room
-  resources :rooms, only: %i[index show create], param: :key do
-    resources :danmus, only: %i[create]
+
+  defaults format: :json do
+    # Rooms
+    resources :rooms, only: %i[index show create], param: :key do
+      # Danmus
+      resources :danmus, only: %i[create]
+    end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
