5420391eb70a1604fad104a58d36b8067e01f033
楊竑昕 <jk195417@gmail.com>
2018-04-03 20:20:03 +0800

    Sender: add methods combind redis

--- app/models/sender.rb
+++ app/models/sender.rb
@@ -1,3 +1,26 @@
 class Sender < ApplicationRecord
   has_many :danmus
+
+  def last_action
+    $redis.get "#{id}_last_action"
+  end
+
+  def last_action=(action)
+    @redis.set "#{id}_last_action", action.to_s
+  end
+
+  def room_key
+    $redis.get "#{id}_room_key"
+  end
+
+  def room_key=(key)
+    $redis.set "#{id}_room_key", key.to_s
+    self.last_action = 'set_room_key'
+  end
+
+  def danmu(text)
+    room = Room.find_by!(key: room_key)
+    danmu.create!(room: room, content: text)
+    self.last_action = 'danmu'
+  end
 end
