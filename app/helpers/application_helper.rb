2aad09d896ab6ef6453eb3b8f833d270a4fa4e3d
楊竑昕 <jk195417@gmail.com>
2018-11-12 08:43:34 +0800

    bootstrap alert + rails flash message

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def bootstrap_alerts
+    types = { notice: :success, alert: :danger }
+    cloes_btn = content_tag(:button, 'x', class: :close, 'data-dismiss': :alert)
+    alerts = flash.map do |type, message|
+      content = cloes_btn + message
+      style = types.fetch(type.to_sym, 'secondary')
+      klass = "alert alert-#{style} alert-dismissable"
+      content_tag(:div, content, class: klass)
+    end
+    alerts.join("\n").html_safe
+  end
 end
