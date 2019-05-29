8e24ee9c09975ab8e26f5853884d56dcbb7b1511
楊竑昕 <jk195417@gmail.com>
2019-05-29 15:28:41 +0800

    Remove empty tests

--- test/controllers/line_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class LineControllerTest < ActionDispatch::IntegrationTest
-  test "should get callback" do
-    get line_callback_url
-    assert_response :success
-  end
-
-end
