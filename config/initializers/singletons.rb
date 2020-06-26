8fbc3f7d8016621a6e1868efa10e81ebb672ddba
楊竑昕 <jk195417@gmail.com>
2020-06-26 23:09:17 +0800

    Setup pusher credentials and create a singleton

--- config/initializers/singletons.rb
+++ config/initializers/singletons.rb
@@ -2,10 +2,11 @@
 
 class Singletons
   include Singleton
-  attr_reader :redis, :line_bot
+  attr_reader :redis, :line_bot, :pusher
 
   def initialize
     @redis = Redis.new(url: Rails.application.config.redis_url)
     @line_bot = Line::Bot::Client.new(Rails.application.config.line_bot)
+    @pusher = Pusher::Client.new(Rails.application.config.pusher)
   end
 end
